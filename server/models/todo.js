15c0d8ff9199cacde91ff022864361311c40e9d2
Sebastian Garbarek <sebastian.garbarek@hotmail.co.nz>
2017-12-24 17:14:32 +1300

    Form initial structure

---
+++ server/models/todo.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo', {
+  task: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  done: {
+    type: Boolean,
+    default: false
+  },
+  doneAt: {
+    type: Number
+    default: null
+  }
+});
+
+module.exports = {Todo};
