9aebb481cdb7cacdbad0ff88aea77722f5c09056
Sebastian Garbarek <sebastian.garbarek@hotmail.co.nz>
2017-12-29 18:28:39 +1300

    Add user join tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -14,11 +14,12 @@ describe('todo', () => {
   beforeEach(populateTodos);
 
   describe('GET /todo', () => {
-    it('should respond with all the todos in the database', (done) => {
+    it('should respond with all todos in the database', (done) => {
       chai.request(app)
         .get('/todo')
         .end((err, res) => {
           res.body.todos.should.have.lengthOf(seedTodos.length);
+
           done();
         });
     });
@@ -33,6 +34,7 @@ describe('todo', () => {
           Todo.find().then((databaseTodos) => {
             databaseTodos.should.have.lengthOf(seedTodos.length);
           }).catch((err) => done(err));
+
           done();
         });
     });
@@ -43,6 +45,7 @@ describe('todo', () => {
         .send({})
         .end((err, res) => {
           res.should.have.status(400);
+
           done();
         });
     });
@@ -65,6 +68,7 @@ describe('user', () => {
           res.should.have.status(200);
           res.body._id.should.equal(seedUsers[0]._id.toHexString());
           res.body.email.should.equal(seedUsers[0].email);
+
           done();
         });
     });
@@ -75,13 +79,68 @@ describe('user', () => {
         .end((err, res) => {
           res.should.have.status(401);
           res.body.should.deep.equal({});
+
           done();
         });
     });
   });
 
   describe('POST /join', () => {
+    it('should add a new user', (done) => {
+      var req = {
+        email: 'test@test.com',
+        password: 'password'
+      };
 
+      chai.request(app)
+        .post('/join')
+        .send(req)
+        .end((err, res) => {
+          if (err) return done(err);
+
+          res.should.have.status(200);
+          res.headers['x-auth'].should.exist;
+          res.body._id.should.exist;
+          res.body.email.should.exist;
+
+          // Wrap the email in an object for findOne().
+          var email = req.email;
+          User.findOne({email}).then((user) => {
+            user.should.exist;
+            user.password.should.not.equal(req.password);
+          });
+
+          done();
+        });
+    });
+
+    it('should return a validation error if the join request was invalid', (done) => {
+      chai.request(app)
+        .post('/join')
+        .send({
+          email: 'invalid',
+          password: 'password'
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+
+          done();
+        });
+    });
+
+    it('should not add a user if the email has already been validated', (done) => {
+      chai.request(app)
+        .post('/join')
+        .send({
+          email: seedUsers[0].email,
+          password: 'password'
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+
+          done();
+        });
+    });
   });
 
   afterEach((done) => {
