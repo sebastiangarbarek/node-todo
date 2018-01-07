7542e142146478edc06aeeeb4594065b62d7e059
Sebastian Garbarek <sebastian.garbarek@hotmail.co.nz>
2018-01-07 13:32:31 +1300

    Add function to update todo by id

--- server/controllers/todo-controller.js
+++ server/controllers/todo-controller.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const ObjectID = require('mongodb').ObjectID;
 
 var Todo = require('../models/todo');
@@ -57,5 +58,24 @@ exports.deleteTodo = (req, res, next) => {
 };
 
 exports.patchTodo = (req, res, next) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['task', 'completed']);
 
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) return res.status(404).send();
+    res.send({todo});
+  }).catch((err) => {
+    next(err);
+  });
 };
